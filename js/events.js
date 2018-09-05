1c4588b908b2bf509eda3f05ad5c4d898edbce12
Matthew Etress <mattetress@me.com>
2018-09-05 05:09:00 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  } ) };
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })};
+
+function pressIt() {
+  $('input#typing').on('keydown', function(pressed) {
+    if (pressed.key == "g" || pressed.key == "G") {
+      alert("Hold up! You pressed the 'G' key!!");
+    }
+  })
+};
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+};
 
 $(document).ready(function(){
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 // call functions here
 
 });
